6069b302eafd50a8710cf093c5294176a742047d
elliot-mb <elliotbuckingham14@gmail.com>
2024-01-22 18:10:07 +0000

    further crashes added

--- simple_crash.js
+++ simple_crash.js
@@ -1,5 +1,5 @@
 
-const CRASH_ID = 2;
+const CRASH_ID = 6;
 
 switch(CRASH_ID){
     case 0:
@@ -13,7 +13,7 @@ switch(CRASH_ID){
             const x = 'a';
             const y = 0;
             const z = x - y;
-            console.log(z); //c is NaN which is not a number which is not a value (so it has gone wrong)
+            console.log(z); //c is NaN which is not a number which is not a value (goes wrong)
         }
         break;
 
@@ -22,7 +22,30 @@ switch(CRASH_ID){
             const x = () => 0;
             const y = 1;
             const z = x + y;
-            console.log(z); // gives a string, undefined behaviour 
+            console.log(z); // gives a string, undefined behaviour (goes wrong)
+        }
+        break;
+    case 3:
+        {
+            const x = null;
+            const y = 1;
+            x(y); // crash: null is not a funtion (goes wrong)
+        }
+        break;
+    case 4: 
+        {
+            console.log([][1]); //returns undefined, undefined behaviour (goes wrong)
+        }
+        break;
+    case 5:
+        {
+            console.log([][undefined]); //returns undefined, both this and the last are because array syntax is also json syntax (goes wrong)
+        }
+        break;
+    case 6:
+        {
+            const f = (f) => f(f); 
+            f(f); //diverges
         }
         break;
     default:
