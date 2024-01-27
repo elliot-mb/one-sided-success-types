b1cd18d9fb0daea4428cd90e304a6a473257eaaa
elliot-mb <elliotbuckingham14@gmail.com>
2024-01-27 17:44:30 +0000

    added json stringifyier that properly indents and creates newlines

--- subset_draft/example_terms.js
+++ subset_draft/example_terms.js
@@ -14,8 +14,9 @@ import {toASTTree} from './aw_ast.js';
  * M, N ::= x | n | n + m | n - m | [M, N] | 
  *          x => M | M(N) | M <= 0 ? N : P 
  * 
- * 
  * f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v))) --i can have recursion with the Z combinator
+ * we define this with Z ::= f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v)));
+ * 
  * 
  * 
  * evaluation contexts 
@@ -115,6 +116,29 @@ import {toASTTree} from './aw_ast.js';
 // testing AST correspondences
 const showProg = (program) => console.log(JSON.stringify(toASTTree(program)));
 
+const pretifyy = json => `{\r\n${prettify(json, '  ')}\r\n}`;
+const prettify = (json, whitespace = '') => {  // gives an array of lines to the tree_file
+    const isArr = Array.isArray(json); //a flag to stop us printing object key names if we are an array(array indices)
+    
+    const ks = Object.keys(json);
+    if(ks.length === 0) return [];
+
+    const keys_copy = ks.map(k => `${k}`) ;
+    const toKey = (i) => isArr ? '' : `"${keys_copy[i]}": `; //we can stop outputting keys if we are an array
+
+    const lines = ks //just the lines that show all the nested objects
+        .map(key => json[key]) //the nested objects
+        .map((value, i) => Array.isArray(value) //is this value an array
+            ? [`${whitespace}${toKey(i)}[\r\n`] + prettify(value, whitespace + `  `) + [`\r\n${whitespace}],`]
+            : typeof(value) === 'object' && value !== null
+                ? [`${whitespace}${toKey(i)}{\r\n`] + prettify(value, whitespace + `  `) + [`\r\n${whitespace}},`]
+                : `${whitespace}${toKey(i)}${JSON.stringify(value)},`); //stringify handles whether we need quotes
+            // : typeof(value) === 'array' 
+            //     ? console.log('array')
+                
+    return lines.reduce((acc, x) => `${acc}\r\n${x}`);
+};
+
 // showProg('1 <= 0 ? 2 : 3');
 
 /**
@@ -142,7 +166,12 @@ console.log(1 - (x => x)); //NaN
 console.log(1 + (x => x)); //type conversion (to string)
 console.log((x => x)()); //undefined
 console.log((y => y + 2)([0, 2])) //type conversion (to string)
-console.log((f => (x => f(x(x)))(x => f(x(x))))(y => y)) //call stack size exceeded (divergence)
+try{
+    console.log((f => (x => f(x(x)))(x => f(x(x))))(y => y)) //call stack size exceeded (divergence)
+}catch(err){
+    console.log(`this term caused a crash: '${err.message}'`)
+}
+
 
 const Y = f => (x => f(x(x)))(x => f(x(x)));
 const Z = f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v)));
@@ -150,3 +179,14 @@ const add = f => x => y => y <= 0 ? x : (f(x + 1)(y - 1));
 const addFix = Z(add); //recursion is real! 
 
 console.log(addFix(2)(20));
+
+/**
+ * language checker using ast: are we using the correct subset?
+ */
+
+const f = './tree_file.json';
+await Bun.write(f, JSON.stringify(toASTTree('f => x => y => y <= 0 ? x : (f(x + 1)(y - 1))')));
+
+const inspection = toASTTree('x => x');
+console.log(pretifyy(inspection))
+console.log(inspection);
