37b1d9aae97132d673e8b09c5514f2677128a15e
elliot-mb <elliotbuckingham14@gmail.com>
2024-02-10 17:22:56 +0000

    update readme

--- subset_draft/typevar.js
+++ subset_draft/typevar.js
@@ -11,9 +11,10 @@ import {Utils} from './utils.js';
 export class GenT{
     static type = 'typevar';
     static genShape = 'A';
+    static okShape = 'Ok';
     static numShape = 'Num';
     static arrowShape = 'A -> B';
-
+    
     constructor(id){
         this.id = id;
         this.shape = () => GenT.genShape;
@@ -69,6 +70,21 @@ export class GenT{
 // meaning: all values 
 export class OkT extends GenT{
 
+    constructor(){
+        super(GenT.okShape);
+        this.shape = () => GenT.okShape;
+    }
+
+    freeIn(){
+        return []; //nothing free in Ok
+    }
+
+    //you cant subtitute with an Ok!
+    swapWith(tA, tB){ 
+        Utils.typeVarsOrCrash(tA, tB);
+        return this;
+    }
+
 }
 
 export class NumT extends GenT{
@@ -76,10 +92,6 @@ export class NumT extends GenT{
     constructor(){
         super(GenT.numShape);
         this.shape = () => GenT.numShape;
-    }
-
-    show(){
-        return this.shape();
     }
 
     freeIn(){
