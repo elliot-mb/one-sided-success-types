38569b5de12831ed97d313b1decdf4aec2ea46a4
elliot-mb <elliotbuckingham14@gmail.com>
2024-02-20 16:55:15 +0000

    adding node support for scripts

---
+++ subset_draft/wrapper_z3.js
@@ -0,0 +1,29 @@
+
+/**
+ * idiom for importing z3 solver 
+ * source: https://www.npmjs.com/package/z3-solver
+ */
+// import { init } from 'z3-solver';
+// const {
+//   Context, // High-level Z3Py-like API
+// } = await init();
+// const { Solver, Int, And } = new Context('main');
+
+
+/**
+ * examples from the api
+ * https://microsoft.github.io/z3guide/programming/Z3%20JavaScript%20Examples/
+ */
+const main = async () => {
+    const { init } = require('z3-solver');
+    const { Context } = await init();
+    const Z3 = new Context('z3');
+    const solver = new Z3.Solver();
+
+    const x = Z3.Bool.const('x');
+
+    solver.add(Z3.And(x, x));
+    solver.check().then(result => console.log(result));
+}
+
+main();
