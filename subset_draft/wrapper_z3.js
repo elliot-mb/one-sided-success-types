7aec41fe9ff0058f1fe9730b9a1f963ea7bb9018
elliot-mb <elliotbuckingham14@gmail.com>
2024-02-21 17:03:38 +0000

    got a solution, but finding all seems inefficient

--- subset_draft/wrapper_z3.js
+++ subset_draft/wrapper_z3.js
@@ -16,40 +16,45 @@ import {modRequire} from './module_require.js';
  * examples from the api
  * https://microsoft.github.io/z3guide/programming/Z3%20JavaScript%20Examples/
  */
+
 const main = async () => {
     const { init } = modRequire('z3-solver');
     const { Context, Z3l } = await init();
     const Z3 = new Context('z3');
 
+    const boolVal = async (bool) => {
+        const solver = new Z3.Solver();
+        solver.add(bool);
+        const isSat = await (solver.check().then(isSat => isSat === 'sat'));
+        return isSat;
+    }
+
 
     const solver = new Z3.Solver();
     const x = Z3.Bool.const('x');
     const y = Z3.Bool.const('y');
-    const conjecture = Z3.And(x, y);
+    const conjecture = Z3.Or(x, Z3.Not(y));
     solver.add(conjecture);
     const is_sat = await solver.check();
     console.log(is_sat);
 
     if(is_sat === 'sat'){
         const model = solver.model();
-
-        const xSol = model.get(x);
-        const ySol = model.get(y);
-
         //console.log(model.decls());
 
         const ds = model.decls();
-        const vs = ds.map(d => {
-            const term = d.call();
-            return model.eval(term, true);
-        });
-        //console.log(vs);
-        Promise.all(vs.map(async v => {
-            const solver = new Z3.Solver();
-            solver.add(v);
-            const isSat = await (solver.check().then(isSat => isSat === 'sat'));
-            return isSat;
-        })).then(tvs => console.log(tvs));
+        const vs = {}; 
+        await Promise.all(ds.filter(d => d.arity() === 0).map(async d => { //list of pairs of names and vals
+            const term = d.call(); //all declarations are evaluated
+            vs[d.name()] = await boolVal(model.eval(term, true));
+        }));
+        console.log(vs);
+        // Promise.all(vs.map(async v => {
+        //     const solver = new Z3.Solver();
+        //     solver.add(v[1]);
+        //     const isSat = await (solver.check().then(isSat => isSat === 'sat'));
+        //     return isSat;
+        // })).then(tvs => console.log(tvs));
         // console.log(truthVals);
     }
 
