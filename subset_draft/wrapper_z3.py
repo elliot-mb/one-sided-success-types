6d951687a946e3a3c0479df7284ff35c89f35731
elliot-mb <elliotbuckingham14@gmail.com>
2024-03-04 18:41:10 +0000

    python runner works

--- subset_draft/wrapper_z3.py
+++ subset_draft/wrapper_z3.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from z3 import *
 
 parser = argparse.ArgumentParser(description = 'z3 interface')
 parser.add_argument('-constraints', '-c', type=str, default=None)
@@ -7,8 +8,27 @@ args = parser.parse_args()
 
 def main():
     constrs = json.loads(args.constraints)
-
-    print(constrs)
+    reply = {
+        'reflect': constrs,
+        'sol': None
+    }
+    solver = Solver()
+    JSTy = Datatype('JSTy')
+    JSTy.declare('Num')
+    JSTy.declare('Ok')
+    JSTy.declare('Comp', ('val', JSTy))
+    JSTy.declare('To', ('lft', JSTy), ('rgt', JSTy))
+    JSTy = JSTy.create()
+    a = Const('a', JSTy)
+    b = Const('b', JSTy)
+    c = Const('c', JSTy)
+    
+    #solve(b != JSTy.To(a, c))
+    solver.add(Or(And(b == JSTy.To(a, c), (a == JSTy.Num)), (JSTy.Ok == b)))
+    solver.check()
+    mod = solver.model()
+    reply['sol'] = list(map(lambda x: {str(x): str(mod[x])}, mod)) 
+    print(reply) #test to see if we can send the constraints back
 
 if __name__ == '__main__':
     main()
