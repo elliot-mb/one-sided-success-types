d7b87b53ea2ff30c469a12e8747d09ecc74cc34e
elliot-mb <elliotbuckingham14@gmail.com>
2024-05-06 13:56:55 +0100

    lunch time

--- demo/prog.js
+++ demo/prog.js
@@ -1,2 +1,16 @@
-const id = (x => x);
-id(0);
+const id = x => x;
+const mightFail = x => {
+    return x <= 0 ? 1 : 2(3);
+}
+const guardFail = x => {
+    return (y => y) + x ? 1 : 2;
+}
+const willFail = x => {
+    return x <= 0 ? (0 <= 0 ? 0(0) : 0(0)) : id + id; 
+}
+const mgw = mightFail(guardFail(willFail));
+const gmw = guardFail(mightFail(willFail));
+const wgm = willFail(guardFail(mightFail));
+const mNum = mightFail(0-1);
+const gNum = guardFail(0); 
+const wNum = willFail(0);  
